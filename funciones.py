0d8c25abe4063a38da0b983b1d41f4fdc45f1be3
cuateverde <ikermarcor5@gmail.com>
2022-04-29 12:13:47 -0700

    añadí un inicio de sesión, portada e ventana de inicio para los usuarios

---
+++ funciones.py
@@ -0,0 +1,15 @@
+import csv
+
+def lee_diccionario_csv(archivo:str)->list:
+    '''Lee un archivo CSV y regresa un diccionario de diccionarios
+    '''
+    diccionario = {}
+    try:
+        with open(archivo,'r',encoding='utf-8') as fh:
+            csv_reader = csv.DictReader(fh)
+            for renglon in csv_reader:
+                llave = renglon['usuario']
+                diccionario[llave]=renglon
+    except IOError:
+        print(f"No se pudo leer el archivo {archivo}")
+    return diccionario
--- vetApp.py
+++ vetApp.py
@@ -1,20 +1,57 @@
-
-
-from flask import Flask, render_template,request
-
+from flask import Flask, render_template,request,redirect,session
+from funciones import lee_diccionario_csv
+#from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+app.secret_key = "asdfvfñfes7u2nairfn"
+diccionario_usuarios = lee_diccionario_csv('usuarios.csv')
 
-@app.route("/", methods= ['GET','POST'])
+@app.route("/")
 def index():
-    if request.method == 'GET':
-        return render_template('Signin.html')
-    
+    return render_template("index.html")
 
-@app.route('/templates/Inicio.html', methods = ['POST'])
+#LOGIN entrar a la pagina con el usuario registrado
+@app.route("/login/", methods= ["GET","POST"])
+def login():
+    if request.method == 'GET':
+        msg = ''
+        return render_template('signin.html',mensaje=msg)
+
+    elif request.method == 'POST':
+            usuario = request.form['usuario']
+            #print(diccionario_usuarios)
+            if usuario in diccionario_usuarios:
+                password_db = diccionario_usuarios[usuario]['secreto'] # password guardado
+                password_forma = request.form['contrasenia'] #password presentado
+                #verificado = sha256_crypt.verify(password_forma,password_db)
+                if (password_db == password_forma):
+                    session['usuario'] = usuario
+                    session['logged_in'] = True
+                    if 'ruta' in session:
+                        ruta = session['ruta']
+                        session['ruta'] = None
+                        return redirect(ruta)
+                    else:
+                        return redirect("/inicio")
+                else:
+                    msg = f'El password de {usuario} no corresponde'
+                    return render_template('signin.html',mensaje=msg)
+            else:
+                msg = f'usuario {usuario} no existe'
+                return render_template('signin.html',mensaje=msg)
+
+#LOGOUT salir de la pagina
+@app.route('/logout/', methods=['GET'])
+def logout():
+    if request.method == 'GET':
+        session.clear()
+        return redirect("/")
+    
+#Inicio o vista del usuario
+@app.route('/inicio', methods = ['GET'])
 def Inicio():
-    if request.method == 'POST':
-        return render_template("Inicio.html", correo = request.form['correo'])
+    if request.method == 'GET':
+        return render_template('inicio.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
